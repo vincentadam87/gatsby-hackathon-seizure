d504f6154659fc6b3f331ca34d9d4e274e2aa2a2
Shaun Dowling <Shaun@dhcp-49-122.xlate.eduroam.ucl.ac.uk>
2014-06-28 13:57:28 +0100

    Add simple training instance class.

---
+++ code/python/seizures/data/Instance.py
@@ -0,0 +1,6 @@
+class Instance(object):
+
+    def __init__(self, latency, eeg_data, frequency):
+        self.latency = latency
+        self.eeg_data = eeg_data
+        self.frequency = frequency
