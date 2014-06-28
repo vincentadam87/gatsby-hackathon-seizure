3226833916375c403237b6aa7cfd4a53137c9044
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-06-28 14:10:01 +0100

    random classifier evaluation working

--- code/python/seizures/evaluation/auc.py
+++ code/python/seizures/evaluation/auc.py
@@ -1,5 +1,6 @@
 from sklearn import metrics
 
+
 def auc(truth, prediction):
     """
     Compute the AUC for given predictions and ground truth
@@ -10,8 +11,8 @@ def auc(truth, prediction):
     
     @author: Heiko
     """
-    # compute tp & fp rates
-    fpr, tpr, _ = metrics.roc_curve(truth, prediction)
+    # compute tp & fp rates, 1 is regarded as positive
+    fpr, tpr, _ = metrics.roc_curve(truth, prediction, pos_label=1)
     
     # return integrated ROC curve
     return metrics.auc(fpr, tpr)
---
+++ code/python/seizures/tests/xvalidation_random_classifier.py
@@ -0,0 +1,19 @@
+'''
+Created on 28 Jun 2014
+
+@author: heiko
+'''
+import numpy as np
+from seizures.evaluation.XValidation import XValidation
+from seizures.prediction.RandomPredictor import RandomPredictor
+
+
+if __name__ == '__main__':
+    predictor = RandomPredictor()
+    
+    N=1000
+    D=2
+    X=np.random.randn(N,D)
+    y=np.random.randint(0,2,N)
+    
+    print XValidation.evaluate(X, y, predictor, n_iter=2, test_size=0.5)
