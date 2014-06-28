1dd7806dc2068fea606443383c807a64e689bbb5
Shaun Dowling <Shaun@dhcp-49-122.xlate.eduroam.ucl.ac.uk>
2014-06-28 14:07:16 +0100

    Explain instance.

--- code/python/seizures/data/Instance.py
+++ code/python/seizures/data/Instance.py
@@ -1,5 +1,12 @@
 class Instance(object):
+    """
+    A simple data structure for one training instance.
 
+    eeg_data - 2d numpy array of channels x time
+
+    @author: Shaun
+    """
+    
     def __init__(self, patient_id, latencies, eeg_data, sample_rate):
         self.patient_id = patient_id
         self.latencies = latencies
