6362563f29197cd04ef536bdb09a6bd667bf52fc
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-06-28 13:27:07 +0100

    base class for our predictors

---
+++ code/python/seizures/prediction/PredictorBase.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+
+
+class PredictorBase(object):
+    """"
+    Abstract base class that implement the interface that we use for our
+    predictors. Classic supervised learning.
+    
+    @author: Heiko
+    """
+
+    @abstractmethod
+    def fit(self, X, y):
+        """
+        Method to fit the model.
+        
+        Parameters:
+        X - 2d numpy array of training data
+        y - 1d numpy array of training labels
+        """
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def predict(self, X, y):
+        """
+        Method to apply the model data
+        
+        Parameters:
+        X - 2d numpy array of test data
+        """
+        raise NotImplementedError()
