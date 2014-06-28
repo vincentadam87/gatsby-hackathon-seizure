6ac380dc1020813346e4b37b5222ef643c381e2d
Julian <julian@julian-K53SC.(none)>
2014-06-28 17:47:24 +0100

    Adjusted FFTFeatures class to subsample data internally

--- code/python/seizures/features/FFTFeatures.py
+++ code/python/seizures/features/FFTFeatures.py
@@ -6,19 +6,20 @@ from seizures.features.FeatureExtractBase import FeatureExtractBase
 
 class FFTFeatures(FeatureExtractBase):
     """
-    Extracts spectral powers based on FFT features
-    
+    Class to extracts spectral powers based on FFT features.
     @author Julian
     """
 
-    def __init__(self, bins):
+    def __init__(self, sampling_rate=400, bins=10):
+        self.sampling_rate = sampling_rate
         self.bins = bins
 
     def extract(self, eeg_data_instance):
-        features = np.empty((eeg_data_instance.number_of_channels, self.bins))
+        subsampled_eeg_data = eeg_data_instance.subsample_data(self.sampling_rate)
+        features = np.empty((subsampled_eeg_data.number_of_channels, self.bins))
 
-        for channel_index in range(0, eeg_data_instance.number_of_channels):
-            frequencies = abs(fftshift(fft(eeg_data_instance.eeg_data[channel_index, :])))
+        for channel_index in range(0, subsampled_eeg_data.number_of_channels):
+            frequencies = abs(fftshift(fft(subsampled_eeg_data.eeg_data[channel_index, :])))
             frequencies_to_sum = len(frequencies) / (self.bins * 2)
 
             for i in range(1, self.bins):
