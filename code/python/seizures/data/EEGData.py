595c9dec22c9793a413f2c35eb37772c6b9db073
Matthieu Louis <matthieu.a.louis@gmail.com>
2014-06-28 15:49:08 +0100

    Fixed some bugs in the EEGData class

--- code/python/seizures/data/EEGData.py
+++ code/python/seizures/data/EEGData.py
@@ -9,17 +9,23 @@ class EEGData:
     """
     Simple object that contains the EEG_data
 
+    path : path to the stitched (concatenated) data.
+
+
     eeg_data : the actual eeg_data
     sampling_rate : sampling rate of the data
     latency : latency in seconds [0 - 31]
     number_of_channels : number of channels in the data
 
     """
+
     def __init__(self, path):
         full_data = scipy.io.loadmat(path)
         self.eeg_data = full_data['data']
-        self.latency = np.array(full_data['latency'])[0, :]
-        self.sampling_rate = self.eeg_data.shape[1]/len(self.latency)
+        if 'latency' in full_data:
+            self.latency = np.array(full_data['latency'])[0, :]
+        if 'freq' in full_data:
+            self.sampling_rate = round(full_data['freq'][0, 0])
         self.number_of_channels = self.eeg_data.shape[0]
 
     def get_time_channel_slice(self, channels=None, low_second=None, high_second=None):
@@ -50,6 +56,9 @@ class EEGData:
             
         self.eeg_data = new_eeg_data
         self.sampling_rate = new_sampling_rate
+
+
+
 
     def get_instances(self):
         instancesList = list()
--- code/python/seizures/data/Instance.py
+++ code/python/seizures/data/Instance.py
@@ -1,7 +1,7 @@
 class Instance(object):
 
-    def __init__(self, patient_id, latencies, eeg_data, sample_rate):
+    def __init__(self, patient_id, latency, eeg_data, sample_rate):
         self.patient_id = patient_id
-        self.latencies = latencies
+        self.latency = latency
         self.eeg_data = eeg_data
         self.sample_rate = sample_rate
