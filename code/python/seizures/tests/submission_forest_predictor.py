6a1b88c2e7554e5b06372892aaf7d774b304cd13
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-06-28 23:15:29 +0100

    added forest predictor submission test

---
+++ code/python/seizures/tests/submission_forest_predictor.py
@@ -0,0 +1,20 @@
+'''
+Created on 28 Jun 2014
+
+@author: heiko
+'''
+from seizures.features.RandomFeatures import RandomFeatures
+from seizures.prediction.ForestPredictor import ForestPredictor
+from seizures.submission.SubmissionFile import SubmissionFile
+
+
+if __name__ == '__main__':
+    predictor = ForestPredictor()
+    extractor = RandomFeatures()
+    
+    test_files = ["Dog_1_test_segment_1.mat"]
+    data_path = "/home/heiko/data/seizure/"
+    
+    submission = SubmissionFile(data_path)
+    submission.generate_submission(predictor, predictor,
+                            extractor, test_filenames=test_files)
