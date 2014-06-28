abb18d43360e41c85a99a9f48e0359819f9ebd3a
Shaun Dowling <Shaun@dhcp-49-122.xlate.eduroam.ucl.ac.uk>
2014-06-28 14:02:05 +0100

    Add patient_id.

--- code/python/seizures/data/Instance.py
+++ code/python/seizures/data/Instance.py
@@ -1,6 +1,7 @@
 class Instance(object):
 
-    def __init__(self, latency, eeg_data, frequency):
-        self.latency = latency
+    def __init__(self, patient_id, latencies, eeg_data, sample_rate):
+        self.patient_id = patient_id
+        self.latencies = latencies
         self.eeg_data = eeg_data
-        self.frequency = frequency
+        self.sample_rate = sample_rate
