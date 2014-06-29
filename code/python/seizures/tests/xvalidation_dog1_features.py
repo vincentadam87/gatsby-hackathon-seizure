ace5d5493d1ae47452c62f6db46ab5c762786dd1
Shaun Dowling <Shaun@dhcp-49-122.xlate.eduroam.ucl.ac.uk>
2014-06-29 01:10:59 +0100

    Add the ARFeatures.

--- code/python/seizures/tests/xvalidation_dog1_features.py
+++ code/python/seizures/tests/xvalidation_dog1_features.py
@@ -6,6 +6,7 @@ Created on 28 Jun 2014
 from seizures.data.DataLoader import DataLoader
 from seizures.evaluation.XValidation import XValidation
 from seizures.evaluation.performance_measures import accuracy
+from seizures.features.ARFeatures import ARFeatures
 from seizures.features.FFTFeatures import FFTFeatures
 from seizures.helper.data_path import get_data_path
 from seizures.prediction.ForestPredictor import ForestPredictor
@@ -18,21 +19,25 @@ def test_predictor(predictor_cls):
     
     data_path = get_data_path("data_path.txt")
 
-    feature_extractor = FFTFeatures()
+    feature_extractor = ARFeatures()
     
     loader = DataLoader(data_path, feature_extractor)
     X_list = loader.training_data("Dog_1")
     y_list = loader.labels("Dog_1")
 
     print XValidation.evaluate(X_list, y_list[0], predictor, evaluation=accuracy)
-    print XValidation.evaluate(X_list, y_list[1], predictor, evaluation=accuracy)
+
+    # Set the conditioned results for proper evaluation
+    conditioned = [a * b for (a, b) in zip(y_list[0], y_list[1])]
+    print XValidation.evaluate(X_list, conditioned, predictor, evaluation=accuracy)
 
 if __name__ == '__main__':
-    print "RandomPredictor"
-    test_predictor(RandomPredictor)
+
+    # print "RandomPredictor"
+    # test_predictor(RandomPredictor)
 
     print "ForestPredictor"
     test_predictor(ForestPredictor)
- 
+
     print "SVMPredictor"
     test_predictor(SVMPredictor)
