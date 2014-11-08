51e009ede1922d4f4eed41e71c7ae2a7544a1c98
vincentadam87 <vincent.adam87@gmail.com>
2014-11-08 14:31:52 +0000

    A class to track paths : data / code / output

---
+++ code/python/seizures/data/Paths.py
@@ -0,0 +1,9 @@
+class Path2Data(object):
+
+    def __init__(self, python_root, clips_folder, my_result_folder):
+        pass
+
+
+    path_dict['python_root'] = '/nfs/nhome/live/vincenta/git/gatsby-hackathon-seizure/code/python'
+    path_dict['clips_folder'] = '/nfs/data3/kaggle_prediction/'
+    path_dict['my_result_folder'] = '/nfs/nhome/live/vincenta/Desktop/seizures_final_final/'
