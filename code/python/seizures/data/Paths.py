0336ee8bc289f0d624b93b1c666b75677fa1f698
vincentadam87 <vincent.adam87@gmail.com>
2014-11-08 14:32:44 +0000

    A class to keep track of paths to data/code/output

--- code/python/seizures/data/Paths.py
+++ code/python/seizures/data/Paths.py
@@ -1,9 +1,28 @@
-class Path2Data(object):
+class Paths(object):
+
+    """"
+    A class to declare the paths to data / code and outputs
+    """
 
     def __init__(self, python_root, clips_folder, my_result_folder):
-        pass
+        self.python_root = python_root
+        self.clips_folder = clips_folder
+        self.my_result_folder = my_result_folder
 
+    def setPaths(self, **kwargs):
+        if 'python_root' in kwargs:
+            self.python_root = kwargs['python_root']
+        if 'clips_folder' in kwargs:
+            self.python_root = kwargs['clips_folder']
+        if 'my_result_folder' in kwargs:
+            self.python_root = kwargs['my_result_folder']
 
-    path_dict['python_root'] = '/nfs/nhome/live/vincenta/git/gatsby-hackathon-seizure/code/python'
-    path_dict['clips_folder'] = '/nfs/data3/kaggle_prediction/'
-    path_dict['my_result_folder'] = '/nfs/nhome/live/vincenta/Desktop/seizures_final_final/'
+    def get_python_root(self):
+        return self.python_root
+
+    def get_clips_folder(self):
+        return self.clips_folder
+
+    def get_my_result_folder(self):
+        return self.my_result_folder
+
