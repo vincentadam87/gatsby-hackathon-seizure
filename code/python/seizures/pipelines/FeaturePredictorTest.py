4e3c6499079b809f5917acf4540bfb87388343fa
Joana Soldado Magraner <joana.magraner.13@ucl.ac.uk>
2015-07-22 13:48:03 +0100

    edit4

--- code/python/seizures/pipelines/FeaturePredictorTest.py
+++ code/python/seizures/pipelines/FeaturePredictorTest.py
@@ -18,13 +18,13 @@ import numpy as np
 import time
 
 
-from independent_jobs.tools.Log import Log
-from independent_jobs.aggregators.SingleResultAggregator import SingleResultAggregator
-from independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
-from independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
-from independent_jobs.jobs.IndependentJob import IndependentJob
-from independent_jobs.results.SingleResult import SingleResult
-from independent_jobs.tools.Log import logger
+from independent-jobs.independent_jobs.tools.Log import Log
+from independent-jobs.independent_jobs.aggregators.SingleResultAggregator import SingleResultAggregator
+from independent-jobs.independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
+from independent-jobs.independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
+from independent-jobs.independent_jobs.jobs.IndependentJob import IndependentJob
+from independent-jobs.independent_jobs.results.SingleResult import SingleResult
+from independent-jobs.independent_jobs.tools.Log import logger
 
 class Data_parallel_job(IndependentJob):
     def __init__(self, aggregator,feature_extractor,x,params):
--- code/python/seizures/tests/parallel_pred.py
+++ code/python/seizures/tests/parallel_pred.py
@@ -2,13 +2,13 @@ from code.python.seizures.data.DataLoader_pred import DataLoader, make_test_labe
 
 import sys
 
-from independent_jobs.tools.Log import Log
-from independent_jobs.aggregators.SingleResultAggregator import SingleResultAggregator
-from independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
-from independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
-from independent_jobs.jobs.IndependentJob import IndependentJob
-from independent_jobs.results.SingleResult import SingleResult
-from independent_jobs.tools.Log import logger
+from independent-jobs.independent_jobs.tools.Log import Log
+from independent-jobs.independent_jobs.aggregators.SingleResultAggregator import SingleResultAggregator
+from independent-jobs.independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
+from independent-jobs.independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
+from independent-jobs.independent_jobs.jobs.IndependentJob import IndependentJob
+from independent-jobs.independent_jobs.results.SingleResult import SingleResult
+from independent-jobs.independent_jobs.tools.Log import logger
 import time
 import numpy as np
 from code.python.seizures.evaluation.performance_measures import auc
